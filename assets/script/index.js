079c19b4a59a0db653db3b1c030c96ce73eb73da
CodeByIshika <ishika11518@gmail.com>
2023-11-10 19:05:49 -0600

    Update responsiveness

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,16 +1,8 @@
 'use strict';
 
-// Utility functions
-// function onEvent(event, selector, callback) {
-//     return selector.addEventListener(event, callback);
-// }
-    
-// function select(selector, parent = document) {
-//     return parent.querySelector(selector);
-// }
-
 
 document.addEventListener('DOMContentLoaded', function() {
     var langElement = document.querySelector('.lang');
     langElement.classList.add('fade-in');
-  });
+});
+

8761588da853ac12bdec94dd4530df75b8d9863e
CodeByIshika <ishika11518@gmail.com>
2023-11-10 19:40:37 -0600

    Update index.html

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,8 +1,3 @@
 'use strict';
 
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var langElement = document.querySelector('.lang');
-//     langElement.classList.add('fade-in');
-// });
-
